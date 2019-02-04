53525f0535de86e95213ef24abace5676f4dc2a7
Florent Becouze <florentbecouze@live.fr>
2019-02-04 15:08:04 +0100

    Début projet

---
+++ Code/app/src/main/java/com/example/ollie/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ollie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView text = findViewById(R.id.text);
+        final Button bouton = findViewById(R.id.bouton);
+
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("Florent et Clément");
+            }
+        });
+
+    }
+
+}
