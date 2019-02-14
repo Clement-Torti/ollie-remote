8a8c3f69e975436a2028754d3f4e2d6372b8a4af
Florent Becouze <florentbecouze@live.fr>
2019-02-14 11:50:16 +0100

    Ajout du logo de l'appli ; Création du fichier strings(fr) ; Ajout de couleurs

--- Code/app/src/main/java/com/example/ollie/MainActivity.java
+++ Code/app/src/main/java/com/example/ollie/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ollie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Vue
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+
+
         setContentView(R.layout.activity_main);
 
         final TextView text = findViewById(R.id.text);
